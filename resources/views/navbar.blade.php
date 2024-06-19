32e32be6609e71e3dcf7198bf63e3479d2c7b3e4
jamiefayezsksk <@jimejamie76@gmail.com>
2024-06-19 22:57:33 +0800

    updated navigation bar to buttons

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,11 +8,15 @@
     }
     .navbar a {
         float: left;
-        display: block;
+        display: inline-block;
         color: #f2f2f2;
         text-align: center;
         padding: 14px 20px;
         text-decoration: none;
+        transition: background-color 0.3s;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        margin-right: 5px; /* Add margin to separate buttons */
     }
     .navbar a:hover {
         background-color: #ddd;
@@ -22,9 +26,8 @@
         padding: 20px;
     }
 </style>
-
 <div class="navbar">
-    <a href="{{ route('about.me') }}">About Me</a>
-    <a href="{{ route('skills') }}">Skills</a>
-    <a href="{{ route('hobbies') }}">Hobbies</a>
+    <a href="{{ route('about.me') }}" class="button">About Me</a>
+    <a href="{{ route('skills') }}" class="button">Skills</a>
+    <a href="{{ route('hobbies') }}" class="button">Hobbies</a>
 </div>
